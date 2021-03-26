fa95db63c4667fa1f22ac4b621d46cb1284a8586
stefano <stefano.ivancich@studenti.unipd.it>
2021-03-26 16:23:08 +0100

    Readme database

--- database-postgres/scripts/data.sql
+++ database-postgres/scripts/data.sql
@@ -1,5 +1,18 @@
+/* INSERT */
+
+
+/* INSERT CUSTOMERS */
 INSERT INTO customer (id, calls_count, email, password, plan, username)
-VALUES (1, 50, 'email.com', 'password', 'FREE', 'username');
+VALUES (1, 50, 'email.com', 'password', 'FREE', 'username1');
 
 INSERT INTO customer (id, calls_count, email, password, plan, username)
-VALUES (2, 50, 'email2.com', 'password', 'FREE', 'username');
+VALUES (2, 50, 'email2.com', 'password', 'FREE', 'username2');
+
+INSERT INTO customer (id, calls_count, email, password, plan, username)
+VALUES (3, 150, 'email3.com', 'password', 'FREE', 'username3');
+
+/* INSERT ORDERS */
+
+
+INSERT INTO customer (calls_count, email, password, plan, username)
+VALUES (150, 'email3.com', 'password', 'FREE', 'username3');
