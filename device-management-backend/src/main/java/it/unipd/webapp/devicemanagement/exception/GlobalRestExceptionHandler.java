94992a09bb64b81cb704f7d76a37097776e420b0
Luca <lucamoro96@gmail.com>
2021-03-29 18:44:21 +0200

    Generic REST exception handler

---
+++ device-management-backend/src/main/java/it/unipd/webapp/devicemanagement/exception/GlobalRestExceptionHandler.java
@@ -0,0 +1,73 @@
+package it.unipd.webapp.devicemanagement.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.postgresql.util.PSQLException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalRestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    /*
+    Handler for not @Valid requests.
+     */
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status, WebRequest request
+    ) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", status.value());
+
+        //Get all fields errors
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(x -> x.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        body.put("errors", errors);
+
+        return new ResponseEntity<>(body, headers, status);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomErrorResponse> resourceNotFoundHandler(Exception ex, WebRequest request) {
+
+        var errors = CustomErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .error(ex.getMessage())
+                .status(HttpStatus.NOT_FOUND.value())
+                .build();
+
+        return new ResponseEntity<>(errors, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(PSQLException.class)
+    public ResponseEntity<CustomErrorResponse> pgExceptionHandler(PSQLException ex, WebRequest request) {
+        var errors = CustomErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .error("Database error")
+                .message(ex.getMessage()) // TODO remove this on "production" - we shouldn't expose internal errors
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .build();
+
+        return new ResponseEntity<>(errors, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
