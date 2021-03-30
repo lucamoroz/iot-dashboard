0d849375b56db2f71768f45a622900a4bbf533bc
Fabio Vaccaro <fabiovaccaro1@gmail.com>
2021-03-30 16:02:58 +0200

    GET request /devices/{id}/data
    - Modified SensorDataController to add new request
    - Modified SensorDataRepository to add method to query SensorData given the device id

--- device-management-backend/src/main/java/it/unipd/webapp/devicemanagement/repository/SensorDataRepository.java
+++ device-management-backend/src/main/java/it/unipd/webapp/devicemanagement/repository/SensorDataRepository.java
@@ -1,6 +1,10 @@
 package it.unipd.webapp.devicemanagement.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import it.unipd.webapp.devicemanagement.model.SensorData;
@@ -8,6 +12,8 @@ import it.unipd.webapp.devicemanagement.model.SensorData;
 @Repository
 public interface SensorDataRepository extends JpaRepository<SensorData, Long> {
     
+    @Query(value = "SELECT id, value, timestamp, device_id, data_type_id FROM sensor_data WHERE device_id = :deviceId ORDER BY timestamp LIMIT :limit", nativeQuery = true)
+    public Optional<List<SensorData>> getDeviceDataByDeviceId(Long deviceId, int limit);
 }
 
 
