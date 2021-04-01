86bbcb41805cb12500cc6fac25559920f55f3b79
Francesco <pham.francesco@gmail.com>
2021-04-01 17:22:10 +0200

    add device and delete device

--- device-management-backend/src/main/java/it/unipd/webapp/devicemanagement/controller/DeviceController.java
+++ device-management-backend/src/main/java/it/unipd/webapp/devicemanagement/controller/DeviceController.java
@@ -2,9 +2,7 @@ package it.unipd.webapp.devicemanagement.controller;
 
 import antlr.Token;
 import it.unipd.webapp.devicemanagement.exception.ResourceNotFoundException;
-import it.unipd.webapp.devicemanagement.model.ClientMessage;
-import it.unipd.webapp.devicemanagement.model.Device;
-import it.unipd.webapp.devicemanagement.model.DeviceConfig;
+import it.unipd.webapp.devicemanagement.model.*;
 import it.unipd.webapp.devicemanagement.repository.DeviceRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
-import it.unipd.webapp.devicemanagement.model.Customer;
 
 @RestController
 @Slf4j
@@ -70,6 +68,50 @@ public class DeviceController {
         deviceConfig.setToken(tokenGenerator.nextToken());
         repository.save(device);
         ClientMessage clientMessage = new ClientMessage("New token generated for device id: "+ deviceId);
+        return ResponseEntity.ok(clientMessage);
+    }
+
+    @PostMapping("/device")
+    public ResponseEntity<ClientMessage> addDevice(
+            @RequestParam long productId,
+            @RequestParam long updateFrequency,
+            @RequestParam boolean enabled,
+            @RequestParam float latitude,
+            @RequestParam float longitude) {
+
+        DeviceConfig deviceConfig = new DeviceConfig();
+        deviceConfig.setToken(tokenGenerator.nextToken());
+        deviceConfig.setUpdate_frequency(updateFrequency);
+        deviceConfig.setEnabled(enabled);
+        deviceConfig.setLatitude(latitude);
+        deviceConfig.setLongitude(longitude);
+
+        DeviceStatus deviceStatus = new DeviceStatus();
+        deviceStatus.setBattery((byte)100);
+        deviceStatus.setVersion("x.y.z");
+        deviceStatus.setLast_update(new Date());
+
+        Device device = new Device();
+        Customer loggedCustomer = (Customer) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        device.setCustomer(loggedCustomer);
+        device.setConfig(deviceConfig);
+        // TODO: add setProduct when ProductRepository will be merged
+
+        repository.save(device);
+        ClientMessage clientMessage = new ClientMessage("New device added");
+        return ResponseEntity.ok(clientMessage);
+    }
+
+    @DeleteMapping("/device/{id}")
+    public ResponseEntity<ClientMessage> deleteDeviceById(@PathVariable(value = "id") long deviceId)
+            throws ResourceNotFoundException {
+        log.info("deleteDeviceById");
+        Customer loggedCustomer = (Customer) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Device device = repository.findCustomerDeviceById(loggedCustomer.getId(), deviceId).
+                orElseThrow(() -> new ResourceNotFoundException("user's device not found for id: " + deviceId));
+        repository.delete(device);
+        // TODO: remove references of this device in other tables
+        ClientMessage clientMessage = new ClientMessage("Deleted device id: "+ deviceId);
         return ResponseEntity.ok(clientMessage);
     }
 }
