a01845318dfa99a0ee42b3727f4f1beca0440ad9
stefano <stefano.ivancich@studenti.unipd.it>
2021-04-03 18:21:31 +0200

    simulation

--- user-simulation/test.py
+++ user-simulation/test.py
@@ -2,11 +2,13 @@ import requests
 
 root="http://localhost:8080"
 auth=("username1","password")
+
 x = requests.get(root+"/order/cartInfo",auth=auth,params={})
+
 print(x.status_code)
-#print(x.text)
 print(x.json())
 
+#print(x.text)
 
 # User that sign up
 #TODO....
