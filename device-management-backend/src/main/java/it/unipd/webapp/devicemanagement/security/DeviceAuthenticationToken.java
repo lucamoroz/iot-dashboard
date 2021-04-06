7cda8f5e27660db90c4679640dd0d68a8c7392d9
Luca <lucamoro96@gmail.com>
2021-04-06 15:39:26 +0200

    added authenticated Device instance to DeviceAuthenticationToken

--- device-management-backend/src/main/java/it/unipd/webapp/devicemanagement/security/DeviceAuthenticationToken.java
+++ device-management-backend/src/main/java/it/unipd/webapp/devicemanagement/security/DeviceAuthenticationToken.java
@@ -1,28 +1,32 @@
 package it.unipd.webapp.devicemanagement.security;
 
+import it.unipd.webapp.devicemanagement.model.Device;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Collection;
 
 public class DeviceAuthenticationToken extends AbstractAuthenticationToken {
-    private final String deviceId;
+    private final Long deviceId;
     private final String token;
+    private final Device device;
 
-    DeviceAuthenticationToken(final String deviceId, final String token) {
+    DeviceAuthenticationToken(final Long deviceId, final String token) {
         super(null);
 
         this.deviceId = deviceId;
         this.token = token;
+        this.device = null;
 
         setAuthenticated(false);
     }
 
-    DeviceAuthenticationToken(final String deviceId, final String token, final Collection<? extends GrantedAuthority> authorities) {
+    DeviceAuthenticationToken(final Long deviceId, final String token, final Collection<? extends GrantedAuthority> authorities, final Device device) {
         super(authorities);
 
         this.deviceId = deviceId;
         this.token = token;
+        this.device = device;
 
         setAuthenticated(true);
     }
@@ -35,5 +39,9 @@ public class DeviceAuthenticationToken extends AbstractAuthenticationToken {
     @Override
     public Object getPrincipal() {
         return deviceId;
+    }
+
+    public Device getDevice() {
+        return device;
     }
 }
