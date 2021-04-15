bd48fbe27b26ead0358cea4de72fb86aa42dce70
Luca <lucamoro96@gmail.com>
2021-04-15 10:56:27 +0200

    import clean up

--- device-management-backend/src/main/java/it/unipd/webapp/devicemanagement/exception/GlobalRestExceptionHandler.java
+++ device-management-backend/src/main/java/it/unipd/webapp/devicemanagement/exception/GlobalRestExceptionHandler.java
@@ -3,22 +3,14 @@ package it.unipd.webapp.devicemanagement.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.postgresql.util.PSQLException;
 import org.postgresql.util.ServerErrorMessage;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @ControllerAdvice
 @Slf4j
