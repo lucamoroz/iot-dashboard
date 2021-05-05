5abd4a35d53854c2a126b86c37272b8a048c777a
Stefano <ivancich.stefano@gmail.com>
2021-05-05 21:54:56 +0200

    cartInfo
    doesn update this.state.data

--- frontend/src/pages/ShopCart.js
+++ frontend/src/pages/ShopCart.js
@@ -8,11 +8,64 @@ class ShopCart extends React.Component {
         super(props);
 
         this.state = {
-            error: false,
-            dataLabels: [],
-            tableRows: [],
-            deviceStatus: null,
-            config: null,
+            error:false,
+            data:"",
         };
+
+        console.log("constructor");
+    }
+
+    componentDidMount() {
+        console.log("didmount");
+        axios.get('/order/cartInfo').then((res) => {
+            console.log("cartinfo");
+            console.log(res.data);
+            this.setState({
+                error: false,
+                data:res.data,
+            });
+        })
+        .catch((error) => {
+            console.log(error.response);
+
+            this.setState({
+                error: true,
+                data:"",
+            });
+        });
+
+    }
+
+
+    render() {
+        
+        console.log("render");
+        if (this.state.error) {
+            
+            return (
+                <span>Error Loading data</span>
+            );
+        } else {
+            console.log(this.state);
+            const element = <h1>Hello, {this.state}</h1>;
+            return (
+                element
+            );
+        }
     }
 }
+
+export default ShopCart
+
+/*
+
+TODO:
+- buy cart button. PostMapping("/buyCart"). orderId, orderAddress
+- get cart info. @GetMapping("/cartInfo")
+- edit product quantity. PostMapping("/editProductQuantity"). productId, newQuantity
+- remove product from cart. DeleteMapping("/removeProductFromCart/{id}")
+- 
+
+
+
+*/
