02c05df1189184c70d22acd31c4c6cc096afdf75
Francesco <pham.francesco@gmail.com>
2021-05-05 20:12:54 +0200

    add dashboard component

---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const axios = require('axios').default
+
+class Dashboard extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>Dashboard</div>
+        );
+    }
+}
+
+export default Dashboard
