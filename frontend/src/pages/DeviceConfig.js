58a654e46dd01da5310493f624e6026cb89847e6
Nicola Maino <nicola.maino.1@studenti.unipd.it>
2021-05-08 15:48:51 +0200

    attempt of retrieving group by device id

--- frontend/src/pages/DeviceConfig.js
+++ frontend/src/pages/DeviceConfig.js
@@ -1,13 +1,71 @@
 import React from "react";
 
-// const axios = require('axios').default
+const axios = require('axios').default
 
-function DeviceConfig() {
-    return (
-        <div>
-            ciao
-        </div>        
-    );
+const DeviceId = (props) => (
+	<div>
+  	    deviceID: {props.id}
+	</div>
+);
+
+class DeviceGroups extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            errorState: false,
+            groups: [],
+        }
+    }
+
+    getGroups() {
+        axios.get('devices/'+this.props.id)
+        .then((resp) => {
+            this.setState({
+                deviceGroups: resp.groups[0].name,
+            })
+        })
+        .catch((error) => {
+            //Sets error state
+            this.setState({
+                errorState: true,
+            })
+        })
+    } 
+
+    render() {
+        this.getGroups();
+        console.log("'devices/'+this.props.id");
+        if (this.state.errorState) {
+            return (
+                <span>Error Loading data</span>
+            );
+        }
+        else {
+            return (
+                <div>
+                    group1={this.state.groups}
+                </div>
+            );
+        }
+    }
+}
+
+
+class DeviceConfig extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div>
+                <DeviceId id="5" />
+                <DeviceGroups />
+            </div>        
+        );
+    }
 }
 
 export default DeviceConfig
