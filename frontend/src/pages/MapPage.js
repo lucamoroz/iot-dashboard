7be23d6cd8799bf11fe8e1316790dbadd47284ba
Denis Deronjic <deronjic.denis@gmail.com>
2021-05-09 13:14:15 +0200

    improved mapPage

--- frontend/src/pages/MapPage.js
+++ frontend/src/pages/MapPage.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { featureGroup, LatLngBounds, latLngBounds } from 'leaflet';
 
 const axios = require('axios').default
 
@@ -25,49 +26,62 @@ const useStyles = makeStyles((theme) => ({
 
 
 
-//Not optimized for network calls. For every render it calls the axiom.get function. TODO: Solve this by using react component
 export default function MapPage() {
   const classes = useStyles();
-  const [devices, setDevices] = useState([]);
+  const [state, setState] = useState({devices: [], bounds: []});
+  const center = [51.505, -0.09] // Just a 
   useEffect(() => {
     axios.get("/devices")
       .then((res) => {
         console.log(res);
-        setDevices(res.data);
+        var mbounds = [];
+        res.data.forEach(device => {
+          mbounds.push([device.device.config.latitude, device.device.config.longitude]);
+        });
+        console.log(mbounds);
+       
+        setState({devices: res.data, bounds: mbounds});
+        
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
+  function ChangeMapBounds({ bounds }) {
+    const map = useMap();
+    bounds.push([51.505, -0.09]); // Mockup bound just to avoid a crash
+    
+    var newBounds = latLngBounds(bounds);
+    map.fitBounds(newBounds);
+    return null;
+  }
+
   return (
     <div className={classes.root}>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className={classes.mapContainer}>
+      <MapContainer center={center} minZoom={0} maxZoom={13} zoom={6} scrollWheelZoom={false} markerZoomAnimation={true} className={classes.mapContainer}>
+      <ChangeMapBounds bounds={state.bounds}/> 
         <TileLayer
           attribution=''
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          
         {
-          devices.forEach((device, i) => {
-            //console.log(device.device.config);
-            var config = device.device.config;
-            var lat = config.latitude;
-            var lon = config.longitude;
-            console.log(lat);
-            console.log(lon);
-            return (
-              <Marker position={[51.505, -0.09]}>
-                <Popup>
-                  A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-              </Marker>
-            );
-          })
+          
+          state.devices.map((device) => (
+
+            <Marker key={device.device.id} position={[device.device.config.latitude, device.device.config.longitude]}>
+              <Popup>
+                Lat: {device.device.config.latitude} Lon: {device.device.config.longitude} <br />
+                Freq: {device.device.config.update_frequency} <br />
+                Battery: {device.device.deviceStatus.battery}% <br />
+                Last Update: {device.device.deviceStatus.last_update} <br />
+               
+              </Popup>
+            </Marker>
+          ))
+
         }
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+
 
       </MapContainer>
     </div>
