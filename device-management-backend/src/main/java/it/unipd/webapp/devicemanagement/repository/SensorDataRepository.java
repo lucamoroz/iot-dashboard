67bcfd17c3a92050866732817b916cd76c4a054e
Fabio Vaccaro <fabiovaccaro1@gmail.com>
2021-05-15 17:30:49 +0200

    Last data bug fixed
    The last data was not displayed

--- device-management-backend/src/main/java/it/unipd/webapp/devicemanagement/repository/SensorDataRepository.java
+++ device-management-backend/src/main/java/it/unipd/webapp/devicemanagement/repository/SensorDataRepository.java
@@ -12,7 +12,7 @@ import it.unipd.webapp.devicemanagement.model.SensorData;
 @Repository
 public interface SensorDataRepository extends JpaRepository<SensorData, Long> {
     
-    @Query(value = "SELECT id, value, timestamp, device_id, data_type_id FROM sensor_data WHERE device_id = :deviceId ORDER BY timestamp LIMIT :limit", nativeQuery = true)
+    @Query(value = "SELECT id, value, timestamp, device_id, data_type_id FROM sensor_data WHERE device_id = :deviceId ORDER BY timestamp DESC LIMIT :limit", nativeQuery = true)
     public Optional<List<SensorData>> getDeviceDataByDeviceId(Long deviceId, int limit);
 
     @Query(value = "SELECT * FROM sensor_data AS sd WHERE sd.device_id = :deviceId AND sd.timestamp = (SELECT timestamp FROM sensor_data AS sd WHERE sd.device_id = :deviceId GROUP BY timestamp ORDER BY timestamp DESC LIMIT 1)", nativeQuery = true)
